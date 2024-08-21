2690aa7163db480c6bff0ad8e1d0cc54d2b0019d
Koharu0212 <k.o.19970212@gmail.com>
2024-08-21 11:55:08 +0900

    テスト用のダミーデータを追加

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,45 @@
+export const Seats = 
+[
+	{id: 1,seat_number: 1,user_id: 1},
+	{id: 2,seat_number: 2,user_id: 0},
+	{id: 3,seat_number: 3,user_id: 2},
+	{id: 4,seat_number: 4,user_id: 0},
+	{id: 5,seat_number: 5,user_id: 0},
+	{id: 6,seat_number: 6,user_id: 3},
+	{id: 7,seat_number: 7,user_id: 0},
+	{id: 8,seat_number: 8,user_id: 0},
+	{id: 9,seat_number: 9,user_id: 0},
+	{id: 10,seat_number: 10,user_id: 0},
+	{id: 11,seat_number: 11,user_id: 0},
+	{id: 12,seat_number: 12,user_id: 0},
+	{id: 13,seat_number: 13,user_id: 0},
+	{id: 14,seat_number: 14,user_id: 0},
+	{id: 15,seat_number: 15,user_id: 4},
+	{id: 16,seat_number: 16,user_id: 0},
+	{id: 17,seat_number: 17,user_id: 0},
+	{id: 18,seat_number: 18,user_id: 0},
+	{id: 19,seat_number: 19,user_id: 0},
+	{id: 20,seat_number: 20,user_id: 0},
+	{id: 21,seat_number: 21,user_id: 0},
+	{id: 22,seat_number: 22,user_id: 5},
+	{id: 23,seat_number: 23,user_id: 0},
+	{id: 24,seat_number: 24,user_id: 0},
+	{id: 25,seat_number: 25,user_id: 0}
+];
+
+export const Users =
+[
+	{user_id: 1, username: "kokahana", email:"koharu@rrr.com", password: "koharu0212"},
+	{user_id: 2, username: "inu", email:"inu@zzz.com", password: "inuinu"},
+	{user_id: 3, username: "neko", email:"neko@email.com", password: "nekoneko"},
+	{user_id: 4, username: "sheep", email:"sheep@email.com", password: "hitsuji"},
+	{user_id: 5, username: "usagi", email:"usagi@xxx.com", password: "usausa"},
+];
+
+export const StudyRecord =
+[
+	{id: 1, user_id: 1, study_date: "2024-09-01", measurement_time: "01:02:03", content: "english"},
+	{id: 2, user_id: 1, study_date: "2024-08-01", measurement_time: "00:30:00", content: "test2"},
+	{id: 3, user_id: 1, study_date: "2024-08-23", measurement_time: "00:00:05", content: "words"},
+	{id: 4, user_id: 1, study_date: "2023-01-01", measurement_time: "10:20:30", content: "test test"}
+];
