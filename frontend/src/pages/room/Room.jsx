6594857aac88ef2c6b7345f5ea087f305ce7915c
Koharu0212 <k.o.19970212@gmail.com>
2024-09-04 11:57:39 +0900

    座席情報をDBから取得

--- frontend/src/pages/room/Room.jsx
+++ frontend/src/pages/room/Room.jsx
@@ -1,21 +1,49 @@
 // Room.jsx
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import './Room.css';
 import Topbar from '../../components/topbar/Topbar';
 import Seat from '../../components/seat/Seat';
 import SeatLegend from '../../components/seatLegend/SeatLegend';
 import ModalComponent from '../../components/modalComponent/ModalComponent';
-import { Seats, Users } from '../../dummyData';
+// import { Seats, Users } from '../../dummyData';
 import { ModalContext } from '../../ModalProvider';
+import axios from 'axios';
 
 export default function Room() {
-  const currentUserId = 1;
+  const currentUserId = 1; //ログイン中のuser id
+  const [seats, setSeats] = useState([]);
+  const [users, setUsers] = useState([]);
+
   const {openModal} = useContext(ModalContext);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3001/api/seats`);
+            setSeats(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3001/api/users`);
+            setUsers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    fetchUser();
+  }, []);
   
   const handleSeatClick = (selectedSeat) => {
     const isOccupied = selectedSeat.user_id !== 0;
     const isCurrentUser = selectedSeat.user_id === currentUserId;
-    const occupantName = Users.find(user => user.id === selectedSeat.user_id)?.username;
+    const occupantName = users.find(user => user.user_id === selectedSeat.user_id)?.username;
 
     let message;
     if (isOccupied) {
@@ -32,10 +60,10 @@ export default function Room() {
       <div className="roomContainer">
         <div className="roomLeft">
           <div className="seatContainer">
-            {Seats.map((seat) => {
+            {seats.map((seat) => {
               const isOccupied = seat.user_id !== 0;
               const isMine = seat.user_id === currentUserId;
-              const occupantName = Users.find(user => user.id === seat.user_id)?.username;
+              const occupantName = users.find(user => user.user_id === seat.user_id)?.username;
 
               return (
                 <Seat 
