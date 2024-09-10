b66d9364b6e5b979c40202d1e6a4af86f1159762
Koharu0212 <k.o.19970212@gmail.com>
2024-09-10 12:38:29 +0900

    ログイン状態を参照

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Top from "./pages/top/Top";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Room from "./pages/room/Room";
 import MyPage from './pages/mypage/MyPage';
 import Measurement from './pages/measurement/Measurement'
-import ModalProvider from './ModalProvider';
+import ModalProvider from './state/ModalProvider';
+import { AuthContext } from './state/AuthContext';
 
 function App() {
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Top />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="room" element={<ModalProvider><Room /></ModalProvider>} />
+        <Route path="/login" element={user ? <Navigate to="/room" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/room" /> : <Register />} />
+        <Route path="/room" element={<ModalProvider><Room /></ModalProvider>} />
         <Route path="/mypage/:username" element={<MyPage />} />
         <Route path="/measurement/:username" element={<Measurement />} />
       </Routes>
