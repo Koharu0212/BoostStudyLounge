a854a6c599a8b508e9fd7b4bfa7fbfa3a55a4fb8
Koharu0212 <k.o.19970212@gmail.com>
2024-09-10 12:40:49 +0900

    ログイン機能を作成

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useRef, useContext } from 'react'
 import "./Login.css"
 import { useNavigate } from 'react-router-dom'
+import { loginCall } from '../../actionCalls';
+import { AuthContext } from '../../state/AuthContext';
 
 export default function Login() {
+	const email = useRef();
+	const password = useRef();
+	const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
 	const navigate = useNavigate();
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		loginCall(
+			{
+				email: email.current.value,
+				password: password.current.value,
+			},
+			dispatch
+		)
+	}
+	
 	return (
 		<div className="login">
 			<div className="loginWrapper">
 				<div className="loginLogo">Boost Study Lounge</div>
-				<div className="loginBox">
-					<input type="text" className="loginInput" placeholder="Eメール" />
-					<input type="text" className="loginInput" placeholder="パスワード" />
-					<button className="loginButton">ログイン</button>
-					<button className="loginRegisterButton" onClick={() => navigate('/register')}>アカウント作成</button>
-				</div>
+				<form className="loginBox" onSubmit={(e) => handleSubmit(e)}>
+					<input type="email" className="loginInput" placeholder="Eメール" required ref={email}/>
+					<input type="password" className="loginInput" placeholder="パスワード" required ref={password}/>
+					<button className="loginButton" type="submit">ログイン</button>
+					<button className="loginRegisterButton" type="button" onClick={() => navigate('/register')}>
+						アカウント作成
+					</button>
+				</form>
 			</div>
 		</div>
 	)
