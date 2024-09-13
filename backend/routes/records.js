fb340debb53da7655270e33ed50f6c13e00690e0
Koharu0212 <k.o.19970212@gmail.com>
2024-09-13 11:44:54 +0900

    勉強時間・内容を記録するAPIを追加

--- backend/routes/records.js
+++ backend/routes/records.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../config/database');
+const moment = require('moment');
 
 //勉強記録を取得
 router.get('/:username', (req, res) => {
@@ -30,6 +31,29 @@ router.get('/:username', (req, res) => {
 		);
 	  }
 	)
+  })
+
+  //勉強時間・内容を記録
+  router.post('/:userId', (req, res) => {
+	const user_id = req.params.userId;
+	const start_date = req.body.start_date;
+	const end_date = req.body.end_date;
+	const contents = req.body.contents;
+
+	//計測時間(s)の算出
+	const measurement_time = moment(end_date).diff(moment(start_date), 'second');
+
+	connection.query(
+		'INSERT INTO study_records (user_id, start_date, end_date, measurement_time, contents) VALUES (?, ?, ?, ?, ?)',
+		[user_id, start_date, end_date, measurement_time, contents],
+		(error, results) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).json({ error: 'クエリに失敗しました' });
+			}
+			return res.status(200).json(results);
+		}
+	);
   })
 
   module.exports = router;
