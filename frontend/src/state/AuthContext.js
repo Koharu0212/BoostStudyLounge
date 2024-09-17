36718a729ce9a0fc62976b1062f1b5d11422f899
Koharu0212 <k.o.19970212@gmail.com>
2024-09-17 15:30:48 +0900

    ログイン状態をローカルストレージに保存

--- frontend/src/state/AuthContext.js
+++ frontend/src/state/AuthContext.js
@@ -1,9 +1,9 @@
-import {createContext, useReducer} from "react";
+import {createContext, useReducer, useEffect} from "react";
 import AuthReducer from "./AuthReducer";
 
 //最初のユーザ状態
 const initialState = {
-	user: false,
+	user: JSON.parse(localStorage.getItem("user")) || null,
 	isFetching: false,
 	error:  false,
 };
@@ -12,6 +12,12 @@ export const AuthContext = createContext(initialState);
 
 export const AuthContextProvider = ({ children }) => { //
 	const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+	//ログイン状態をローカルストレージに保管
+	useEffect(() => {
+		localStorage.setItem("user", JSON.stringify(state.user));
+	}, [state.user])
+
 	return <AuthContext.Provider value={{
 		user: state.user,
 		isFetching: state.isFetching,
