6c0556dda497c5ada83789f7b3cf820014c28cd2
Koharu0212 <k.o.19970212@gmail.com>
2024-09-26 09:53:35 +0900

    テーブルの構成を変更

--- backend/initialize.sql
+++ backend/initialize.sql
@@ -20,6 +20,7 @@ CREATE TABLE study_records (
 CREATE TABLE seats (
   seat_id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT,
+  start_time DATETIME,
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 INSERT INTO seats (seat_id, user_id) VALUES (1, NULL),(2, NULL),(3, NULL),(4, NULL),(5, NULL),(6, NULL),(7, NULL),(8, NULL),(9, NULL),(10, NULL),(11, NULL),(12, NULL),(13, NULL),(14, NULL),(15, NULL),(16, NULL),(17, NULL),(18, NULL),(19, NULL),(20, NULL),(21, NULL),(22, NULL),(23, NULL),(24, NULL),(25, NULL);
