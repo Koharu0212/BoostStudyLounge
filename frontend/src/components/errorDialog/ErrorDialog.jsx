78a8be8b6304a2876cdb9a6ba3ad1df6c9f1fb0e
Koharu0212 <k.o.19970212@gmail.com>
2024-09-26 13:18:57 +0900

    エラー時に表示するダイアログを追加

---
+++ frontend/src/components/errorDialog/ErrorDialog.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@mui/material';
+
+export default function ErrorDialog({ open, onClose, title, content }) {
+  return (
+	<Dialog open={open} onClose={onClose}>
+		<DialogContent>
+			<DialogTitle>
+				{title}
+			</DialogTitle>
+			<DialogContentText>
+				{content}
+			</DialogContentText>
+		</DialogContent>
+		<DialogActions>
+			<Button onClick={onClose} autoFocus>
+				閉じる
+			</Button>
+        </DialogActions>
+	</Dialog>
+  );
+}
