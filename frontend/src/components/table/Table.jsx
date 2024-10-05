41140094a8867db7755efa2a19d43102f440f002
Koharu0212 <k.o.19970212@gmail.com>
2024-10-05 11:06:32 +0900

    formatDate, formTime, formatDuarationをdateUtils.jsに移動

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -1,33 +1,8 @@
 import React from 'react'
 import "./Table.css"
+import { formatDate, formatTime, formatDuration } from '../../utils/dateUtils';
 
 export default function Table({records}) {
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}年${month}月${day}日`;
-  };
-
-  const formatTime = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' });
-  };
-
-  const formatDuration = (seconds) => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-    
-    let result = '';
-    if (hours > 0) result += `${hours}時間`;
-    if (minutes > 0) result += `${minutes}分`;
-    if (remainingSeconds > 0 || result === '') result += `${remainingSeconds}秒`;
-    
-    return result;
-  };
-
   return (
     <>
       <table className="studyRecordTable">
--- frontend/src/utils/dateUtils.js
+++ frontend/src/utils/dateUtils.js
@@ -1,4 +1,4 @@
-export function formatDate(date) {
+export function formatDatetime(date) {
 	const pad = (num) => num.toString().padStart(2, '0');
 	const year = date.getFullYear();
 	const month = pad(date.getMonth() + 1);
@@ -6,6 +6,31 @@ export function formatDate(date) {
 	const hours = pad(date.getHours());
 	const minutes = pad(date.getMinutes());
 	const seconds = pad(date.getSeconds());
-  
 	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
+
+  export function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}年${month}月${day}日`;
+  };
+
+  export function formatTime(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' });
+  };
+
+  export function  formatDuration(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+    
+    let result = '';
+    if (hours > 0) result += `${hours}時間`;
+    if (minutes > 0) result += `${minutes}分`;
+    if (remainingSeconds > 0 || result === '') result += `${remainingSeconds}秒`;
+    
+    return result;
+  };
