d3a531038f9222951b995e6b7540687c8f0fe156
Koharu0212 <k.o.19970212@gmail.com>
2024-10-12 21:44:53 +0900

    mysql2の記法に従ってリファクタリング+node-cronを使用して自動離席機能を実装

--- backend/routes/seats.js
+++ backend/routes/seats.js
@@ -1,166 +1,193 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../config/database');
+const pool = require('../config/database');
+const cron = require('node-cron');
+const MAX_STUDY_TIME = 12 * 60 * 60 * 1000; 
 
 //座席情報を取得
-router.get('/', (req, res) => {
-	connection.query(
-	  'SELECT * FROM seats ORDER BY seat_id',
-	  (error, results) => {
-		if (error) {
-			console.log(error);
-			return res.status(500).json({ error: 'クエリに失敗しました' });
+router.get('/', async (req, res) => {
+	let connection;
+	try {
+		connection = await pool.getConnection();
+		const [rows] = await connection.query('SELECT * FROM seats ORDER BY seat_id');
+		return res.status(200).json(rows);
+	} catch (error) {
+		return res.status(500).json({ error: 'クエリに失敗しました' });
+	} finally {
+		if (connection) {
+			connection.release();
 		}
-		return res.status(200).json(results);
-	  }
-	)
-  })
+	}
+  });
 
   //ユーザが着席している席を取得
-  router.get('/:userId', (req, res) => {
-	connection.query(
-		'SELECT seat_id FROM seats WHERE user_id = ?',
-		[req.params.userId],
-		(error, results) => {
-			if(error) {
-				return res.status(500).json({ error: 'クエリに失敗しました' });
-			}
-			return res.status(200).json(results);
+  router.get('/:userId', async (req, res) => {
+	let connection;
+	try {
+		connection = await pool.getConnection();
+		const [rows] = await connection.query('SELECT seat_id FROM seats WHERE user_id = ?', req.params.userId);
+		return res.status(200).json(rows);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: 'クエリに失敗しました' });
+	} finally {	
+		if (connection) {
+			connection.release();
 		}
-	)
+	}
   })
 
   //着席
-  router.put('/occupy', (req, res) => {
+  router.put('/occupy', async (req, res) => {
 	const { userId, seatId } = req.body;
-	connection.beginTransaction((err) => {
-	  if (err) {
-		return res.status(500).json({ error: 'トランザクションの開始に失敗しました' });
-	  }
-  
-	  //席が空いているか確認
-	  connection.query(
-		'SELECT user_id FROM seats WHERE seat_id = ? FOR UPDATE', //他のユーザが着席できないよう排他制御
-		[seatId],
-		(error, results) => {
-		  if (error) {
-			return connection.rollback(() => {
-			  res.status(500).json({ error: 'クエリに失敗しました' });
-			});
-		  }
-  
-		  if (results.length === 0) {
-			return connection.rollback(() => {
-			  res.status(404).json({ error: '指定された席が見つかりません' });
-			});
-		  }
-  
-		  if (results[0].user_id !== null) {
-			return connection.rollback(() => {
-			  res.status(400).json({ error: 'この席は既に占有されています' });
-			});
-		  }
-  
-		  //ユーザーが既に他の席に座っていないか確認
-		  connection.query(
-			'SELECT seat_id FROM seats WHERE user_id = ?',
-			[userId],
-			(error, results) => {
-			  if (error) {
-				return connection.rollback(() => {
-				  res.status(500).json({ error: 'クエリに失敗しました' });
-				});
-			  }
-  
-			  if (results.length > 0) {
-				return connection.rollback(() => {
-				  res.status(400).json({ error: 'ユーザーは既に別の席に座っています' });
-				});
-			  }
-  
-			  //席を更新
-			  connection.query(
-				'UPDATE seats SET user_id = ?, start_time = CURRENT_TIMESTAMP WHERE seat_id = ?',
-				[userId, seatId],
-				(error, results) => {
-				  if (error) {
-					return connection.rollback(() => {
-					  res.status(500).json({ error: 'クエリに失敗しました' });
-					});
-				  }
-  
-				  connection.commit((err) => {
-					if (err) {
-					  return connection.rollback(() => {
-						res.status(500).json({ error: 'コミットに失敗しました' });
-					  });
-					}
-					res.status(200).json({ message: '席の占有に成功しました' });
-				  });
-				}
-			  );
-			}
-		  );
+	let connection;
+	try {
+		connection = await pool.getConnection();
+   		await connection.beginTransaction();
+		const [rows] = await connection.query('SELECT user_id FROM seats WHERE seat_id = ? FOR UPDATE', [seatId]);
+		if (rows.length === 0) {
+			await connection.rollback();
+			return res.status(404).json({ error: '指定された席が見つかりません' });
+			
 		}
+		if (rows[0].user_id !== null) {
+			await connection.rollback();
+			return res.status(400).json({ error: 'この席は既に占有されています' });
+		}
+
+		const [checkOccupy] = await connection.query('SELECT seat_id FROM seats WHERE user_id = ?', [userId]);
+		if (checkOccupy.length > 0) {
+			await connection.rollback();
+			return res.status(400).json({ error: 'ユーザーは既に別の席に座っています' });
+		}
+
+		//席を更新
+		await connection.query(
+			'UPDATE seats SET user_id = ?, start_time = CURRENT_TIMESTAMP WHERE seat_id = ?', 
+			[userId, seatId]
+		);
+		await connection.commit();
+		return res.status(200).json({ message: '着席に成功しました' });
+	} catch (error) {
+		if (connection) {
+			await connection.rollback();
+			return  res.status(500).json({error: 'クエリに失敗しました'});
+		}
+	} finally {
+		if (connection) {
+			connection.release();
+		}
+	}
+});
+
+//離席
+router.put('/vacate', async (req, res) => {
+	const { userId, seatId } = req.body;
+	let connection;
+	try {
+		connection = await pool.getConnection();
+   		await connection.beginTransaction();
+		//指定された席にユーザが座っているか確認
+		const [rows] = await connection.query('SELECT user_id FROM seats WHERE seat_id = ? FOR UPDATE', [seatId]);
+		if (rows.length === 0) {
+			return res.status(404).json({ error: '指定された席が見つかりません' });
+		}
+		if (rows[0].user_id !== userId) {
+			return res.status(400).json({ error: 'この席に指定したユーザが着席していません' });
+		}
+
+		//席の開放
+		await connection.query('UPDATE seats SET user_id = NULL, start_time = NULL WHERE seat_id = ?', [seatId]);
+		await connection.commit();
+		res.status(200).json({ message: '離席に成功しました' });
+	} catch (error) {
+		if (connection) {
+			await connection.rollback();
+			return res.status(500).json({error: 'クエリに失敗しました'});
+		}
+	} finally {
+		if (connection) {
+			connection.release();
+		}
+	}
+});
+
+router.get('/status/:seatId', async (req, res) => {
+	const seatId = req.params.seatId;
+	let connection;
+	try {
+		connection = await pool.getConnection();
+		const [rows] = await connection.query('SELECT user_id, start_time FROM seats WHERE seat_id = ?', [seatId]);
+		if (rows.length === 0) {
+			return res.status(404).json({ error: '指定された席が見つかりません' });
+		}
+		return res.status(200).json(rows[0]);
+	} catch (error) {
+		if (connection) {
+			return  res.status(500).json({error: 'クエリに失敗しました'});
+		}
+	} finally {
+		if (connection) {
+			connection.release();
+		}
+	}
+});
+
+// 定期的に長時間着席しているユーザーをチェックし、自動で離席させる
+cron.schedule('*/5 * * * *', async () => {
+	console.log('自動離席処理を開始します');
+	let connection;
+	try {
+	  connection = await pool.getConnection();
+	  const [occupiedSeat] = await connection.query(
+		'SELECT seat_id, user_id, start_time FROM seats WHERE user_id IS NOT NULL'
 	  );
-	});
+	  for (const seat of occupiedSeat) {
+		const elapsedTime = new Date() - new Date(seat.start_time);
+		if (elapsedTime >= MAX_STUDY_TIME) {
+			console.log(`離席対象発見：userId=${seat.user_id}`);
+		  await autoVacateSeat(seat.seat_id, seat.user_id, seat.start_time);
+		}
+	  }
+	  console.log('自動離席処理を終了します');
+	} catch (error) {
+	  console.error('Error in cron job:', error);
+	} finally {
+	  if (connection) {
+		connection.release();
+	  }
+	}
   });
 
-  //離席
-  router.put('/vacate', (req, res) => {
-	const { userId, seatId } = req.body;
-  
-	// 指定された席にユーザが座っているか確認
-	connection.query(
-	  'SELECT user_id FROM seats WHERE seat_id = ?',
-	  [seatId],
-	  (error, results) => {
-		if (error) {
-		  return res.status(500).json({ error: 'クエリに失敗しました' });
-		}
-  
-		// 存在しない場合
-		if (results.length === 0) {
-		  return res.status(404).json({ error: '指定された席が見つかりません' });
-		}
-  
-		const seat = results[0];
-  
-		// 座っているのが別のユーザ、または誰も座っていない場合
-		if (seat.user_id !== userId) {
-		  return res.status(400).json({ error: 'この席に指定したユーザが着席していません' });
-		}
-  
-		// 席の開放
-		connection.query(
-			'UPDATE seats SET user_id = NULL, start_time = NULL WHERE seat_id = ?',
-			[seatId],
-			(error, results) => {
-			  if (error) {
-				  res.status(500).json({ error: 'クエリに失敗しました' });
-			  }
-			  res.status(200).json({ message: '席の開放に成功しました' });
-			}
-		  );
-		}
+  async function autoVacateSeat(seatId, userId, startTime) {
+	let connection;
+	try {
+	  connection = await pool.getConnection();
+	  await connection.beginTransaction();
+		console.log('離席処理開始');
+	  await connection.query('UPDATE seats SET user_id = NULL, start_time = NULL WHERE seat_id = ? FOR UPDATE', [seatId]);
+		console.log('離席処理完了');
+	  const endTime = new Date();
+	  const measurementTime = Math.min(Math.floor((endTime - new Date(startTime)) / 1000), MAX_STUDY_TIME / 1000);
+		console.log('離席対象ユーザの勉強記録を開始');
+	  await connection.query(
+		'INSERT INTO study_records (user_id, start_date, end_date, measurement_time, contents) VALUES (?, ?, ?, ?, ?)',
+		[userId, startTime, endTime, measurementTime, '着席時間が12時間を超えたため、自動離席しました。']
 	  );
-	});
+	  console.log('記録完了');
+  
+	  await connection.commit();
+	  console.log(`userId${userId}の離席処理が完了しました`);
+	} catch (error) {
+	  if (connection) {
+		await connection.rollback();
+	  }
+	  console.error('Error in autoVacateSeat:', error);
+	} finally {
+	  if (connection) {
+		connection.release();
+	  }
+	}
+  }
 
-	router.get('/status/:seatId', (req, res) => {
-		const seatId = req.params.seatId;
-		connection.query(
-		  'SELECT user_id, start_time FROM seats WHERE seat_id = ?',
-		  [seatId],
-		  (error, results) => {
-			if (error) {
-			  return res.status(500).json({ error: 'クエリに失敗しました' });
-			}
-			if (results.length === 0) {
-			  return res.status(404).json({ error: '指定された席が見つかりません' });
-			}
-			return res.status(200).json(results[0]);
-		  }
-		);
-	  });
-
-  module.exports = router;
+module.exports = router;
