0c6f17b21c743760a2dc22d029d752b0c67ebc76
Koharu0212 <k.o.19970212@gmail.com>
2024-10-14 14:07:38 +0900

    環境変数のスペルミスを修正

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+
+require('dotenv').config();
 
 app.use(cors({
   origin: [process.env.CLIENT_URL],
@@ -10,16 +11,9 @@ app.use(cors({
   optionsSuccessStatus: 200 
 }));
 
-// テンプレートエンジンとしてEJSを設定
 app.set('view engine', 'ejs');
-
-// cssなど静的ファイルの配置場所を設定
 app.use(express.static(path.join(__dirname, 'public')));
-
-//Body-Parserの使用 (クライアントから送信されたデータをreq.body経由で会得、操作できる)
 app.use(express.json());
-
-//POSTパラメータの受け取りに必要
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/auth', require('./routes/auth'));
@@ -28,7 +22,7 @@ app.use('/api/seats', require('./routes/seats'));
 app.use('/api/records', require('./routes/records'));
 
 // サーバを起動
-app.listen(port, () => {
+app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running at ${process.env.SERVER_URL}`);
 });
 
